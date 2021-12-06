1cf48d2f469b1f07e1e071637e8246600ea1691d
Sam <samuel-shin@live.com>
2021-12-06 12:18:11 -0500

    web_app connection for shoppinplist. still need to fix the error http 405 wrong methods

---
+++ Phase 2/webapp_connection/src/main/java/com/example/webapp_connection/WebappConnectionApplication.java
@@ -0,0 +1,14 @@
+package com.example.webapp_connection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+
+@SpringBootApplication
+public class WebappConnectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebappConnectionApplication.class, args);
+    }
+
+}
---
+++ Phase 2/webapp_connection/src/main/java/com/example/webapp_connection/controller/WebController.java
@@ -0,0 +1,38 @@
+package com.example.webapp_connection.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import controller.ListManager;
+import driver.LogInSystem;
+import gateway.LoadingManager;
+
+@RestController
+public class WebController {
+    String currentUser;
+    ListManager listManager;
+
+    public WebController() {
+        currentUser = LogInSystem.logInSys(); // this call initiates the login system
+        listManager = new ListManager(LoadingManager.InventoryListLoader(currentUser), LoadingManager.ShoppingListLoader(currentUser));
+    }
+
+    @PostMapping("/add")
+    public String addItemToShoppingList(@RequestParam String name, @RequestParam int quantity) {
+        listManager.addShoppingListItem(name, quantity);
+        return "Successfully added the item to the list.";
+    }
+
+    @PostMapping("/remove")
+    public String RemoveItemFromShoppingList(@RequestParam int index) {
+        listManager.removeShoppingListItem(index);
+        return "Successfully removed the item from the list.";
+    }
+
+    @GetMapping("/list")
+    public String getShoppingListItems() {
+        return listManager.printShoppingList();
+    }
+}
